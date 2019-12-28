90bb60687bee25178afb6878bb51f5191c449976
Janessa Garrow <janessa.garrow@gmail.com>
2019-12-27 22:59:33 -0700

    Create variables from received state

--- src/pages/battle.js
+++ src/pages/battle.js
@@ -1,18 +1,22 @@
 import React from "react";
 
 export default ({ location }) => {
+    const { trainerName, party, oppParty } = location.state;
+    const trainerType = location.state.selectedTrainer.name;
+    const opponent = location.state.opponent.name;
+
     return (
         <div>
             <h1>Battle Page</h1>
-            <h2>trainerName: {location.state.trainerName}</h2>
-            <p>selectedTrainer: {location.state.selectedTrainer.name}</p>
-            {location.state.party.map(pokemon => {
-                return <p key={pokemon.id}>{pokemon.name}</p>;
-            })}
-            <h2>Opponent: {location.state.opponent.name}</h2>
-            {location.state.oppParty.map(pokemon => {
-                return <p key={pokemon.pokemon.id}>{pokemon.pokemon.name}</p>;
-            })}
+            <h2>trainerName: {trainerName}</h2>
+            <p>selectedTrainer: {trainerType}</p>
+            {party.map(pokemon => (
+                <p key={pokemon.id}>{pokemon.name}</p>
+            ))}
+            <h2>Opponent: {opponent}</h2>
+            {oppParty.map(pokemon => (
+                <p key={pokemon.pokemon.id}>{pokemon.pokemon.name}</p>
+            ))}
         </div>
     );
 };
